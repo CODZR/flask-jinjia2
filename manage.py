ba9ecd6798ac22e20f5e08c1d0747c5761e182b9
CODZR <975336710@qq.com>
2023-11-07 15:41:24 +0800

    fix: stripe

--- manage.py
+++ manage.py
@@ -4,5 +4,9 @@ from flask_cors import CORS
 app = create_app()
 CORS(app, resources=r'/*')
 
+@app.route('/', methods=['GET'])
+def index():
+   return 'ok'
+
 if __name__ == '__main__':
    app.run()
